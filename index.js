bf05e12fabe4287dd35c832ede040db7bcce6707
AMANKANT312 <amankant312@users.noreply.github.com>
2023-12-14 02:23:15 +0530

    first commit
    gift shop flower is completed

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("i am connected");
+
+const b1 = document.getElementsByClassName("b1");
+const b2 = document.getElementsByClassName("b2");
+// const open = () => {
+//   const navmenu = document.getElementsByClassName("nav_main");
+//   console.log("first");
+//   navmenu.style.display = "block";
+//   b1.style.display = "none";
+//   b2.style.display = "block";
+// };
+
+// for svggg
+let vid = document.getElementById("myVideo");
+vid.playbackRate = 1.5;
